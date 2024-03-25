f3600af895a57f792d0bd3023bde021544df0971
adam <adam8@live.nl>
2024-03-25 14:26:49 +0100

    kleine aanpassingen  verbetering merge

---
+++ SomerenConsoleTest/SomerenModel/Deelname.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Deelname
+    {
+        public int ActiviteitId { get; set; }
+        public int Deelnemer {  get; set; }
+    }
+}
